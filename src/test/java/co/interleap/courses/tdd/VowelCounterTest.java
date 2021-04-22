60456ffc9669b1d4e5d6c59ae3bfdb151ed54261
Yash <yashkalia47@gmail.com>
2021-04-22 12:03:12 +0530

    Added Test Cases

--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -2,8 +2,16 @@ package co.interleap.courses.tdd;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class VowelCounterTest {
+    @Test
+    public void shouldCountZeroWithAnEmptyString(){
+        assertEquals(0, new VowelCounter().count(""));
+    }
 
     @Test
-    public void dummy(){}
+    public void shouldCountSingleVowel(){
+        assertEquals(1,new VowelCounter().count("u"));
+    }
 }
